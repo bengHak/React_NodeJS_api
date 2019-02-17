8c69c09fd0867c41e58d7be2a76353c6e52320df
benghak <qkwl4678@naver.com>
2019-02-17 20:29:19 +0900

    add book api (mongoDB)

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: String,
+    author: String,
+    published_date: { type: Date, default: Date.now  }
+    },
+    {
+        collection: 'Books'
+    }
+);
+
+module.exports = mongoose.model('book', bookSchema);
--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/customers', (req,res) => {
+app.get("/api/customers", (req, res) => {
     res.send([
         {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '나동빈',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '홍길동',
-            'birthday': '960305',
-            'gender': '남자',
-            'job': '프로그래머'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '이순신',
-            'birthday': '921205',
-            'gender': '남자',
-            'job': '디자이너'
-          }
+            id: 1,
+            image: "https://placeimg.com/64/64/1",
+            name: "나동빈",
+            birthday: "961222",
+            gender: "남자",
+            job: "대학생"
+        },
+        {
+            id: 2,
+            image: "https://placeimg.com/64/64/2",
+            name: "홍길동",
+            birthday: "960305",
+            gender: "남자",
+            job: "프로그래머"
+        },
+        {
+            id: 3,
+            image: "https://placeimg.com/64/64/3",
+            name: "이순신",
+            birthday: "921205",
+            gender: "남자",
+            job: "디자이너"
+        }
     ]);
 });
+
+// CONNECT TO MONGODB SERVER
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://localhost:27017');
+
+const Book = require('./models/book');
+const router = require('./routes')(app, Book);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
